4862b1cdfb53b027c473393ff741197d5d1ce5b9
Maksim Lotka <lotkamaxim09@yandex.ru>
2022-06-09 22:51:09 +0300

    Добавил новые файлы и обработки для главных страниц сайта

--- aboutCar/cars/views.py
+++ aboutCar/cars/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Страница приложения о машинах")
+
+
+def names(request, car_name: str):
+    return HttpResponse(f"<h1> Статьи про знаменитые машины </h1><p>{car_name}</p>")
