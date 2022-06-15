7ae11a9f73c0f80a15e0909f3a5836bf616cc295
Maksim Lotka <lotkamaxim09@yandex.ru>
2022-06-15 21:54:22 +0300

    (Change) Изменил стандартную БД на postgres БД cars_project

--- aboutCar/aboutCar/settings.py
+++ aboutCar/aboutCar/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'aboutCar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cars_project',
+        'USER': 'twnj',
+        'PASSWORD': 'pass123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -117,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
